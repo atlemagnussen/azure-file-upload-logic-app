dbf677c5f7f013fa3576304a50a34ed4f92a1e73
Atle Magnussen <atlemagnussen@gmail.com>
2019-12-09 20:15:58 +0100

    enhanced

--- src/web/wwwroot/js/uploader.js
+++ src/web/wwwroot/js/uploader.js
@@ -2,12 +2,13 @@
     async post() {
         var oFormElement = document.getElementById("uploader-form");
         var resultElement = oFormElement.elements.namedItem("result");
+        resultElement.value = "Uploading...";
         const formData = new FormData(oFormElement);
         const endpoint = this.sel.value;
         try {
             const response = await fetch(endpoint, {
                 method: "POST",
-                body: formData
+                body: formData,
             });
 
             if (response.ok) {
@@ -16,13 +17,16 @@
 
             resultElement.value = "Result: " + response.status + " " + response.statusText;
         } catch (error) {
-            console.error("Error:", error);
+            console.error(error);
+            resultElement.value = "Error...";
+            var errmsg = "Error..." + error.message;
+            resultElement.value = errmsg;
         }
     }
     init() {
         console.log("init");
         var btn = document.getElementById("btn-uploader");
-        btn.addEventListener("click", async (e) => {
+        btn.addEventListener("click", async e => {
             e.preventDefault();
             await this.post();
         });
